dc057c546b58f13d06fc5b9531afa3c3b6ee1ffd
JPark2416 <115630125+JPark2416@users.noreply.github.com>
2024-02-04 11:21:22 +1300

    adding conditional display of resultTable

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,14 +1,12 @@
 import { formatter } from "../util/investment";
+import { calculateInvestmentResults } from "../util/investment.jsx";
 
-function ResultTable({ resultValue }) {
-  // resultValue Structure
-  // {
-  //   year: i + 1, // year identifier
-  //   interest: interestEarnedInYear, // the amount of interest earned in this year
-  //   valueEndOfYear: investmentValue, // investment value at end of year
-  //   annualInvestment: annualInvestment, // investment added in this year
-  // }
-  console.log(formatter.format(resultValue[0].valueEndOfYear));
+function ResultTable({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
   return (
     <table id="result">
       <thead>
@@ -16,24 +14,24 @@ function ResultTable({ resultValue }) {
           <th>Year</th>
           <th>Investment Value</th>
           <th>Interest (Year)</th>
-          {/* <th>Total Interest</th> */}
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
         </tr>
       </thead>
       <tbody>
-        {resultValue.map((result, resultindex) => {
+        {resultsData.map((yearData) => {
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
           return (
-            <tr key={resultindex}>
-              <td>{result.year}</td>
-              <td>{formatter.format(result.valueEndOfYear)}</td>
-              <td>{formatter.format(result.interest)}</td>
-              {/* <td>
-                {resultValue
-                  .slice(0, resultindex + 1)
-                  .map((res) => res.annualInvestment)
-                  .reduce((acc, current) => {
-                    return acc + current;
-                  })}
-              </td> */}
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
           );
         })}
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,13 +1,61 @@
-function UserInput({ inputLabel, onChangeInput }) {
-  function handleChange() {
-    onChangeInput(event.target.value);
-  }
+function UserInput({ onChangeInput, userInput }) {
+  // function handleChange(event) {
+  //   onChangeInput(event.target.value);
+  // }
 
   return (
-    <div>
-      <label>{inputLabel}</label>
-      <input type="number" onChange={handleChange}></input>
-    </div>
+    // <div>
+    //   <label>{inputLabel}</label>
+    //   <input type="number" onChange={handleChange}></input>
+    // </div>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              onChangeInput("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              onChangeInput("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              onChangeInput("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => onChangeInput("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
   );
 }
 
